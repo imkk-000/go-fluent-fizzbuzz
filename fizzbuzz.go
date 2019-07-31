a65d94dc78ed40f230ce4a053ee48022add90d01
Nattakit Boonyang <nattakit.boonyang@gmail.com>
2019-08-01 01:29:58 +0700

    Edit: move all divisor to slice of int and map

--- fizzbuzz.go
+++ fizzbuzz.go
@@ -7,16 +7,20 @@ import (
 // FizzBuzz - this is the group of FizzBuzz methods
 type FizzBuzz struct{}
 
+var divisorOrderTable = []int{15, 3, 5}
+var ruleTable = map[int]string{
+	15: "FizzBuzz",
+	3:  "Fizz",
+	5:  "Buzz",
+}
+
 // Say - when you play the game
 // you must say "Fizz", "Buzz", "FizzBuzz", or natual number
 func (FizzBuzz) Say(number int) string {
-	switch {
-	case number%15 == 0:
-		return "FizzBuzz"
-	case number%3 == 0:
-		return "Fizz"
-	case number%5 == 0:
-		return "Buzz"
+	for _, divisor := range divisorOrderTable {
+		if number%divisor == 0 {
+			return ruleTable[divisor]
+		}
 	}
 	return strconv.Itoa(number)
 }
